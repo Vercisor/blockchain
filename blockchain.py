071a12e9b8f17c91bcb6bd9c91bd08eb3e95c2df
Vercisor <davidegamba1998@gmail.com>
2021-05-16 09:54:48 +0200

    Add genesis and mine_block methods

--- blockchain.py
+++ blockchain.py
@@ -1,19 +1,25 @@
 
 from block import Block
 
+
 class Blockchain:
 
     def __init__(self):
-        self.chain = []
-    
+        self.chain = [Block.genesis()]
+
     def add_block(self, data):
-        self.chain.append(Block(data))
-    
+        self.chain.append(Block.mine_block(self.chain[-1], data))
+
     def __repr__(self):
         return f'Blockchain: {self.chain} '
+    
+def main():
+    blockchain = Blockchain()
+    blockchain.add_block('one')
+    blockchain.add_block('two')
 
-blockchain = Blockchain()
-blockchain.add_block('one')
-blockchain.add_block('two')
+    print(blockchain)
+    print(f'blockhain.py __name__: {__name__}')
 
-print(blockchain)
+if __name__ == '__main__':
+    main()
