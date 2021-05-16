aab70c6dad12c9c06706217f1dd2aa01ea609998
Vercisor <davidegamba1998@gmail.com>
2021-05-16 09:25:24 +0200

    Add Block and Blockchain classes

---
+++ block.py
@@ -0,0 +1,7 @@
+class Block:
+
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return f'Block - data: {self.data}'
---
+++ blockchain.py
@@ -0,0 +1,19 @@
+
+from block import Block
+
+class Blockchain:
+
+    def __init__(self):
+        self.chain = []
+    
+    def add_block(self, data):
+        self.chain.append(Block(data))
+    
+    def __repr__(self):
+        return f'Blockchain: {self.chain} '
+
+blockchain = Blockchain()
+blockchain.add_block('one')
+blockchain.add_block('two')
+
+print(blockchain)
